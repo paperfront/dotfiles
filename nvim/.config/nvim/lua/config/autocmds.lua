9ca536bb0ed0f986746dd60b5843060338e5ed67
Matthew Lacayo <mlacayo@Matthews-MBP.localdomain>
2024-03-04 17:55:58 -0500

    Added neorg templates and which key support

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -4,3 +4,31 @@
 vim.api.nvim_create_autocmd("BufWinEnter", {
   command = "set formatoptions-=cro",
 })
+
+local file_exists_and_is_empty = function(filepath)
+  local file = io.open(filepath, "r")
+  if file ~= nil then
+    local content = file:read("*all")
+    file:close()
+    return content == ""
+  else
+    return false
+  end
+end
+
+vim.api.nvim_create_autocmd({ "BufNew", "BufNewFile" }, {
+  callback = function(args)
+    local toc = "index.norg"
+
+    vim.schedule(function()
+      if vim.fn.fnamemodify(args.file, ":t") == toc then
+        return
+      end
+      if args.event == "BufNewFile" or (args.event == "BufNew" and file_exists_and_is_empty(args.file)) then
+        vim.api.nvim_cmd({ cmd = "Neorg", args = { "templates", "fload", "journal" } }, {})
+      end
+    end)
+  end,
+  desc = "Load new workspace entries within a Neorg template",
+  pattern = "/Users/mlacayo/neorg_notes/journal/*.norg",
+})
--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -5,22 +5,49 @@ return {
   lazy = true, -- enable lazy load
   ft = "norg", -- lazy load on file type
   cmd = "Neorg", -- lazy load on command
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim", { "pysan3/neorg-templates", dependencies = { "L3MON4D3/LuaSnip" } } },
   config = function()
     require("neorg").setup({
       load = {
+        ["external.templates"] = {},
+        ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+        ["core.integrations.nvim-cmp"] = {},
         ["core.defaults"] = {}, -- Loads default behaviour
         ["core.concealer"] = {}, -- Adds pretty icons to your documents
         ["core.keybinds"] = {
           -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
           config = {
             default_keybinds = true,
+            neorg_leader = "<LocalLeader>",
+            hook = function(keybinds)
+              keybinds.unmap("norg", "n", keybinds.leader .. "id")
+              local wk = require("which-key")
+              wk.register({
+                n = {
+                  name = "New",
+                  n = {
+                    "<cmd>Neorg keybind norg core.dirman.new.note",
+                    "New File",
+                  },
+                },
+                i = {
+                  name = "Insert",
+                  d = { "a<Space>(<C-r>=strftime('%a %d %b %Y')<CR>)<Esc>", "Date" },
+                },
+                j = {
+                  name = "Journal",
+                  t = { "<cmd>Neorg journal today<cr>", "Today" },
+                  p = { "<cmd>Neorg journal yesterday<cr>", "Yesterday" },
+                  n = { "<cmd>Neorg journal tomorrow<cr>", "Tomorrow" },
+                },
+              }, { prefix = "<LocalLeader>" })
+            end,
           },
         },
         ["core.dirman"] = { -- Manages Neorg workspaces
           config = {
             workspaces = {
-              notes = "~/notes",
+              notes = "~/neorg_notes",
               cplusplus = "~/cplusplus",
             },
             default_workspace = "notes",
@@ -28,6 +55,11 @@ return {
         },
         ["core.ui"] = {},
         ["core.ui.calendar"] = {},
+        ["core.journal"] = {
+          config = {
+            use_template = false,
+          },
+        },
       },
     })
   end,
